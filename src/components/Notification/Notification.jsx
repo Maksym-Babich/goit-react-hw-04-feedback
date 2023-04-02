0e8a5cb9fe026420c0d07bb7db34241871b0fbbf
Maksym-Babich <maxus1042@gmail.com>
2023-04-02 15:59:07 +0300

    changed classes to functional components

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Message } from 'components/Notification/Notification.styled';
 
-export class Notification extends Component {
-  PropTypes = {
-    message: PropTypes.string.isRequired,
-  };
-
-  render() {
-    return <Message>{this.props.message}</Message>;
-  }
+export function Notification({ message }) {
+  return <Message>{message}</Message>;
 }
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { Stats, Stat } from 'components/Statistics/Statistics.styled';
 
-export class Statistics extends Component {
-  PropTypes = {
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.string.isRequired,
-  };
-
-  render() {
-    return (
-      <Stats>
-        {Object.keys(this.props).map((key) => {
-          return <Stat key={nanoid()}>{`${key}: ${this.props[key]}`}</Stat>;
-        })}
-      </Stats>
-    );
-  }
+export function Statistics(props) {
+  return (
+    <Stats>
+      {Object.keys(props).map(key => {
+        return <Stat key={nanoid()}>{`${key}: ${props[key]}`}</Stat>;
+      })}
+    </Stats>
+  );
 }
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.string.isRequired,
+};
